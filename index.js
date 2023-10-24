f44e7c89dfaa0ff3f44c93e0ee721b26819a8b1c
Med Ali Naaoui <chayga00@gmail.com>
2023-10-24 06:17:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(cors());
+
+app.get("/test", (req, res) => {
+  res.send("hell yeah!!");
+});
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.DBURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("connected successfully");
+  } catch (error) {
+    if (error) console.log("error from connecting to mongoDb: ", error);
+  }
+};
+connect();
+
+const port = 8001;
+app.listen(process.env.PORT || port, (error) => {
+  if (!error) console.log("server runnig on port: ", port);
+});
